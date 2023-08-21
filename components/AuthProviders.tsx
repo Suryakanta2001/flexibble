b49ce53f59c996ebe5253096994ec008441dd5e9
Suryakanta_2001 <suryakantasinha01@gmail.com>
2023-08-21 18:00:58 +0530

    first commit

---
+++ components/AuthProviders.tsx
@@ -0,0 +1,11 @@
+'use client';
+import React, { useEffect, useState } from 'react'
+import { getProviders, signIn } from 'next-auth/react';
+
+const AuthProviders = () => {
+  return (
+    <div>AuthProviders</div>
+  )
+}
+
+export default AuthProviders
